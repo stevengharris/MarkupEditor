5e41c29200d00b090233c71a3cdcae49ddf7c86e
Oleksandr Shymanskyi <shymanskyi.oleksandr@gmail.com>
2023-08-28 17:02:40 +0200

    Change package version

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MarkupEditor",
     platforms: [
-        .macCatalyst(.v15),
-        .iOS(.v15)
+        .macCatalyst(.v14),
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
