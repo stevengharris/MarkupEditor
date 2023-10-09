6d5b683424cd303fa8b674ec0b45c06294477e02
Steven G. Harris <steven.g.harris@gmail.com>
2023-10-09 13:36:42 -0700

    Fix local image insertion in DemoContentView and SearchableContentView

--- SwiftUIDemo/SearchableContentView.swift
+++ SwiftUIDemo/SearchableContentView.swift
@@ -13,25 +13,23 @@ import MarkupEditor
 /// Displays the MarkupEditorView containing demo.html. By default, the MarkupEditorView shows the MarkupToolbar at the top.
 ///
 /// Acts as the MarkupDelegate to interact with editing operations as needed.
+///
+/// A local png image is packaged along with the rest of the demo app resources for demo purposes only.
+/// Normally, you would want to put resources in a subdirectory of where your html file comes from, or in
+/// a directory that holds both the html file and all of its resources. When you do that, you would specify
+/// `resourcesUrl` when  instantiating MarkupEditorView, so that the \<img src=...> tag can identify
+/// the `src` for the image relative to your html document.
 struct SearchableContentView: View {
 
     @ObservedObject var selectImage = MarkupEditor.selectImage
     @State private var documentPickerShowing: Bool = false
     @State private var demoHtml: String
     
-    // Note that we specify resourcesUrl when instantiating MarkupEditorView so that we can demonstrate
-    // loading of local resources in the edited document. That resource, a png, is packaged along
-    // with the rest of the demo app resources, so we get more than we wanted from resourcesUrl,
-    // but that's okay for demo. Normally, you would want to put resources in a subdirectory of
-    // where your html file comes from, or in a directory that holds both the html file and all
-    // of its resources.
-    private let resourcesUrl: URL? = URL(string: Bundle.main.resourceURL!.path)
-    
     var body: some View {
         VStack(spacing: 0) {
             SearchBar()
                 .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
-            MarkupEditorView(markupDelegate: self, html: $demoHtml, resourcesUrl: resourcesUrl, id: "Document")
+            MarkupEditorView(markupDelegate: self, html: $demoHtml, id: "Document")
         }
         .pick(isPresented: $selectImage.value, documentTypes: MarkupEditor.supportedImageTypes, onPicked: imageSelected(url:), onCancel: nil)
         .onDisappear { MarkupEditor.selectedWebView = nil }
