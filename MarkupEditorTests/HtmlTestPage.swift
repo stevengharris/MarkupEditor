d1ae3b49b9101952ddfc0205ba07ae61c621cd66
Steven G. Harris <steven.g.harris@gmail.com>
2026-01-31 14:03:55 -0800

    Simplify async creation of MarkupWKWebView for testing

---
+++ MarkupEditorTests/HtmlTestPage.swift
@@ -0,0 +1,14 @@
+//
+//  HtmlTestHolder.swift
+//  MarkupEditor
+//
+//  Created by Steven Harris on 1/30/26.
+//
+
+class HtmlTestPage {
+    static let shared = HtmlTestPage()
+    
+    private init() {
+        
+    }
+}
