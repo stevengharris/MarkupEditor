b3faf231fc5525bf88f80d613c91f3bb7dd120dd
Steven G. Harris <steven.g.harris@gmail.com>
2026-01-31 14:04:36 -0800

    Simplify async creation of MarkupWKWebView for testing

--- MarkupEditorTests/HtmlTestPage.swift
+++ MarkupEditorTests/HtmlTestPage.swift
@@ -1,14 +1,39 @@
 //
-//  HtmlTestHolder.swift
+//  HtmlTestPage.swift
 //  MarkupEditor
 //
 //  Created by Steven Harris on 1/30/26.
 //
 
-class HtmlTestPage {
-    static let shared = HtmlTestPage()
+import MarkupEditor
+
+/// A class to hold onto a MarkupWKWebView instance that is in the "ready" state.
+class HtmlTestPage: MarkupDelegate {
     
-    private init() {
-        
+    private var webView: MarkupWKWebView!
+    private var coordinator: MarkupCoordinator!
+    private var continuation: CheckedContinuation<MarkupWKWebView, Never>?
+    
+    init() {}
+    
+    deinit {
+        webView = nil
+        coordinator = nil
+    }
+
+    /// Return the `webView` after `markupDidLoad` is received.
+    func start(delegate: MarkupDelegate? = nil) async throws -> MarkupWKWebView {
+        await withCheckedContinuation { continuation in
+            self.continuation = continuation
+            webView = MarkupWKWebView(markupDelegate: delegate ?? self)
+            coordinator = MarkupCoordinator(markupDelegate: delegate ?? self, webView: webView)
+            webView.setCoordinatorConfiguration(coordinator)
+        }
+    }
+
+    /// Since we marked self as the `markupDelegate`, we receive the `markupDidLoad` message
+    internal func markupDidLoad(_ view: MarkupWKWebView, handler: (()->Void)?) {
+        continuation?.resume(with: .success(view))
+        continuation = nil
     }
 }
