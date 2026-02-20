0939def5bb10ef70c29bcfa6fb29829b018bcba5
Steven G. Harris <steven.g.harris@gmail.com>
2026-02-20 15:09:41 -0800

    Update custom.js for module-based usage and proper ProseMirror usage

--- SharedDemo/custom.js
+++ SharedDemo/custom.js
@@ -5,19 +5,14 @@
 //  Created by Steven Harris on 1/13/24.
 //
 
+import { MU } from "./markup-editor.js"
+
 /**
  * A public method that can be invoked from MarkupWKWebView to return
  * the number of words in the HTML document using a simpleminded approach.
  * Invoking this method requires an extension to MarkupWKWebView.
  */
 MU.wordCount = function() {
-    let wordCount = 0;
-    const styles = ['P', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'CODE'];
-    for (const style of styles) {
-        const elements = document.querySelectorAll(style);
-        for (const element of elements) {
-            wordCount += element.textContent.trim().split(' ').length;
-        }
-    };
-    return wordCount;
+    const text = MU.activeView()?.state.doc.textContent
+    return text ? text.trim().split(/\s+/).filter(Boolean).length : 0
 };
