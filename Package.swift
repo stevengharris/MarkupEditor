411fb63101a8bd68b4d74ed9a86b061c6ab24055
Steven G. Harris <steven.g.harris@gmail.com>
2026-02-20 09:18:02 -0800

    Update SPI platforms and set swiftLanguageModes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -39,5 +39,6 @@ let package = Package(
             name: "SwiftTests",
             dependencies: ["MarkupEditor", "SharedTest"],
             path: "MarkupEditorTests/SwiftTests"),
-    ]
+    ],
+    swiftLanguageModes: [.v5, .v6]
 )
