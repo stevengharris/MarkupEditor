1d634b8342fd181bf8a19a17bd1f708e24d8f993
Steven G. Harris <steven.g.harris@gmail.com>
2026-07-23 11:23:01 -0700

    Hide everything when markup-editor is not defined

--- MarkupEditor/MarkupWKWebView.swift
+++ MarkupEditor/MarkupWKWebView.swift
@@ -428,6 +428,7 @@ public class MarkupWKWebView: WKWebView, ObservableObject {
                 <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
                 <meta name="supported-color-schemes" content="light dark">
                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                <style>markup-editor:not(:defined) { visibility: hidden; }</style>
             </head>
             <body>
                 <markup-editor
