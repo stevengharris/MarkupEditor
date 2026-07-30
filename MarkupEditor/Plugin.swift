346be294151425cefdd40c665e1b3fd6aaeedf9d
Steven G. Harris <steven.g.harris@gmail.com>
2026-07-30 11:05:44 -0700

    Add ext to Plugin

--- MarkupEditor/Plugin.swift
+++ MarkupEditor/Plugin.swift
@@ -12,22 +12,25 @@ public struct Plugin: Codable, Equatable, Hashable {
         lhs.name == rhs.name && lhs.type == rhs.type && lhs.filename == rhs.filename
     }
 
-    public let name: String           // Name to display for the plugin
-    public let type: String           // Plugin type, e.g. "renderer" or "exporter"
-    public let filename: String?      // JS module filename, if backed by one
+    public let name: String         // Name to display for the plugin
+    public let type: String         // Plugin type, e.g. "renderer" or "exporter"
+    public let filename: String?    // JS module filename, if backed by one
+    public let ext: String?         // For an exporter, the file extension to be used by default
 
-    public init(name: String, type: String, filename: String? = nil) {
+    public init(name: String, type: String, filename: String? = nil, ext: String? = nil) {
         self.name = name
         self.type = type
         self.filename = filename
+        self.ext = ext
     }
 
     public nonisolated init(from decoder: any Decoder) throws {
-        enum Keys: String, CodingKey { case name, type, filename }
+        enum Keys: String, CodingKey { case name, type, filename, ext }
         let c = try decoder.container(keyedBy: Keys.self)
         name = try c.decode(String.self, forKey: .name)
         type = try c.decode(String.self, forKey: .type)
         filename = try c.decodeIfPresent(String.self, forKey: .filename)
+        ext = try c.decodeIfPresent(String.self, forKey: .ext)
     }
 
 }
